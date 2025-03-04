cfb62fed306692e4a8e94637a2cdb756d54da1d6
Ahmed Mohamed <albashengineerahmed@gmail.com>
2025-03-04 16:36:06 +0200

    first version

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src", // Set 'src' as the root directory
+  server: {
+    port: 3000, // Runs on http://localhost:3000/
+  },
+});
