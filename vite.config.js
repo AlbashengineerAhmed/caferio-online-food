73a8d2979091e6a464afb4fbe6cc47326a083eba
Ahmed Mohamed <albashengineerahmed@gmail.com>
2025-03-04 21:16:14 +0200

    update some error

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  root: "src", // Set 'src' as the root directory
-  server: {
-    port: 3000, // Runs on http://localhost:3000/
-  },
-});
